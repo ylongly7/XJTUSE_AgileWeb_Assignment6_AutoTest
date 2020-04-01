981401d10386bbb1ad91fe94b7745a0fc03ecab0
Ximing WANG <sighwang@outlook.com>
2020-04-01 16:26:41 +0800

    test add todo

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -5,7 +5,7 @@ const globalVariables = _.pick(global, ['browser', 'expect']);
 
 // puppeteer options
 const options = {
-  headless: true,
+  headless: false,
   timeout: 10000
 };
 
--- test/sample.spec.js
+++ test/sample.spec.js
@@ -1,7 +1,25 @@
-describe('sample test', function () {
-    it('should work', async function () {
-      console.log(await browser.version());
-        
-      expect(true).to.be.true;
+describe('add todo', function () {
+    let page;
+
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://127.0.0.1:7001/');
     });
+  
+    after (async function () {
+      await page.close();
+    });
+
+    it('should have correct title', async function() {
+        expect(await page.title()).to.eql('Puppeteer Demo');
+    })
+    it('should new todo correct', async function() {
+      await page.click('#new-todo', {delay: 500});
+      await page.type('#new-todo', 'new todo item', {delay: 50});
+      await page.keyboard.press("Enter");
+      let todoList = await page.waitFor('#todo-list');
+      const expectInputContent = await page.evaluate(todoList => todoList.lastChild.querySelector('label').textContent, todoList);
+      expect(expectInputContent).to.eql('new todo item');
+    }) 
+
   });
